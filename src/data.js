40bd49cad8eed10a57436c23593442111a07c76e
Elibabah <delias16.hernandez@gmail.com>
2021-11-25 07:11:40 -0600

    enlazando data sede ajusco

--- src/data.js
+++ src/data.js
@@ -1,17 +1,23 @@
 export const ourData = "../data/students.json";
 
 export let alumnasWild = (ourData, sede) => {
-  fetch(ourData)
-    .then((response) => response.json())
-    .then((data) => wildCodeCamp(data, sede))
-    //metodo de objeto para acceder a las keys,darle id a las imagenes
-    .catch((error) => console.log(error));
+    fetch(ourData)
+        .then((response) => response.json())
+        .then((data) => wildCodeCamp(data, sede))
+        //metodo de objeto para acceder a las keys,darle id a las imagenes: Object.keys(nombreDelObjeto)
+        .catch((error) => console.log(error));
 };
 
 //Función para iterar datos
 let wildCodeCamp = (data, sede) => {
-  console.log(data[sede]);
+    console.log(data[sede]);
 };
+
+document.addEventListener("click", function() {
+    document.getElementById("ajus")
+    alumnasWild(ourData, "ajusco");
+});
+
 //ajusco.generacion.primera.estudiantes
 //Funcion para ingresar de screen 2 a 3 de forma dinámica(screen 3 recibe data de cada una de las sedes)
 // Funcion pàra calcular el numero de alumnos registrados en la sede Ajusco
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import { alumnasWild, ourData } from "./data.js"; //Recibe el EXPORT de fetch de data.js
-alumnasWild(ourData, "ajusco");
+
+//alumnasWild(ourData, "iztapalapa")
+//alumnasWild(ourData, "chapultepec")
 
 //------------------------------// Validación usuario-password //------------------------------//
-/*let validacion = document.getElementById("entry");
+let validacion = document.getElementById("entry");
 
-validacion.addEventListener("click", function () {
-  let user = document.getElementById("name").value;
-  let contraseña = document.getElementById("password").value;
-  if (user == "Vania" && contraseña == 7) {
-    document.getElementById("access-screen").hidden = true; // pantalla user
-    document.getElementById("sedes").hidden = false; // pantalla dashboard
-  } else {
-    alert("Acceso denegado");
-  }
-});*/
+validacion.addEventListener("click", function() {
+    let user = document.getElementById("name").value;
+    let contraseña = document.getElementById("password").value;
+    if (user == "Vania" && contraseña == 7) {
+        document.getElementById("access-screen").hidden = true; // pantalla user
+        document.getElementById("sedes").hidden = false; // pantalla dashboard
+    } else {
+        alert("Acceso denegado");
+    }
+});
