24b608728870ccd282d1a24434b95d13a56425ce
mon0macal7 <monhdz07@gmail.com>
2021-11-29 18:28:44 -0600

    Enlace entre el dashboard,screen de sedes y login

--- src/app.js
+++ src/app.js
@@ -1,63 +1,62 @@
 //import { alumnasWild, ourData } from "./data.js"; //Recibe EXPORT de fetch de data.js
 
 //------------------------- Datos por Ajusco ------------------------------//
-let ajuscoSede = document.getElementById("ajus")
-
+let ajuscoSede = document.getElementById("ajus");
 
 ajuscoSede.addEventListener("click", () => {
+  console.log("click en", ajuscoSede);
+  console.log("ajusco");
+  //    alumnasWild(ourData, "ajusco");
 
-    console.log("click en", ajuscoSede)
-    console.log("ajusco")
-        //    alumnasWild(ourData, "ajusco");
+  //-- Guardar data para llevarla a dashboard --//
+  let myData = "ajusco";
+  localStorage.setItem("objectToPass", myData);
 
-    //-- Guardar data para llevarla a dashboard --//
-    let myData = "ajusco";
-    localStorage.setItem('objectToPass', myData);
+  //------ Función abrir sede en dashboard ------//
 
-    //------ Función abrir sede en dashboard ------//
-
-    localStorage.removeItem('objectToPass'); // Limpiar el localStorage
-    window.location.href = "./index.html"; // Abrir dashboard
-    //alumnasWild(ourData, "ajusco");
-    console.log(myData)
+  localStorage.removeItem("objectToPass"); // Limpiar el localStorage
+  window.location.href = "./index.html"; // Abrir dashboard
+  //alumnasWild(ourData, "ajusco");
+  console.log(myData);
 });
 
-
+//----------------------funcion para volver de pantalla dashboard a sedes---------------------//
+/*document.getElementById("exit").addEventListener("click", () => {
+  window.location.href = "./sedes.html";
+});*/
 
 //---------------------- Datos por Chapultepec ---------------------------//
 
-let chapultepecSede = document.getElementById("chapu")
+let chapultepecSede = document.getElementById("chapu");
 chapultepecSede.addEventListener("click", () => {
-    console.log("click en", chapultepecSede)
-        //    alumnasWild(ourData, "chapultepec");
+  console.log("click en", chapultepecSede);
+  //    alumnasWild(ourData, "chapultepec");
 
-    //-- Guardar data para llevarla a dashboard --//
-    let myData = "chapultepec";
-    localStorage.setItem('objectToPass', myData);
+  //-- Guardar data para llevarla a dashboard --//
+  let myData = "chapultepec";
+  localStorage.setItem("objectToPass", myData);
 
-    //------ Función abrir sede en dashboard ------//
-    localStorage.removeItem('objectToPass'); // Limpiar el localStorage
-    window.location.href = "index.html" //Abrir dashboard
-        //    alumnasWild(ourData, "chapultepec");
-    console.log(myData)
+  //------ Función abrir sede en dashboard ------//
+  localStorage.removeItem("objectToPass"); // Limpiar el localStorage
+  window.location.href = "index.html"; //Abrir dashboard
+  //    alumnasWild(ourData, "chapultepec");
+  console.log(myData);
 });
-
-
 
 //----------------------- Datos por Iztapalapa ---------------------------//
 
-let iztapalapaSede = document.getElementById("ixtapa")
+let iztapalapaSede = document.getElementById("ixtapa");
 iztapalapaSede.addEventListener("click", () => {
-    console.log("click en", iztapalapaSede)
-        //    alumnasWild(ourData, "iztapalapa");
+  console.log("click en", iztapalapaSede);
+  //    alumnasWild(ourData, "iztapalapa");
 
-    //-- Guardar data para llevarla a dashboard --//
-    let myData = "iztapalapa";
-    localStorage.setItem('objectToPass', myData);
+  //-- Guardar data para llevarla a dashboard --//
+  let myData = "iztapalapa";
+  localStorage.setItem("objectToPass", myData);
 
-    //------ Función abrir sede en dashboard ------//
-    localStorage.removeItem('objectToPass'); // Limpiar el localStorage
-    window.location.href = "index.html" // Abrir dashboard
-        //    alumnasWild(ourData, "iztapalapa");
-    console.log(myData)
+  //------ Función abrir sede en dashboard ------//
+  localStorage.removeItem("objectToPass"); // Limpiar el localStorage
+  window.location.href = "index.html"; // Abrir dashboard
+  //    alumnasWild(ourData, "iztapalapa");
+  console.log(myData);
 });
